d0d6bf1ea568f7d3d192a128b83494dc6ee18d78
mabgcm <bugucam@gmail.com>
2026-01-14 11:17:34 -0500

    firebae removed

--- lib/admin.ts
+++
@@ -1,8 +0,0 @@
-import type { User } from "firebase/auth";
-
-// TODO: replace with your real admin emails.
-export const ADMIN_EMAILS = ["bugucam@gmail.com"];
-
-export function isAdminUser(user: User | null): boolean {
-    return !!user?.email && ADMIN_EMAILS.includes(user.email);
-}
